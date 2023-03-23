552b73ae1e07da8aef45893f0dddb6fd564db914
Sebastian <sebastian.v.a@riseup.net>
2023-03-23 21:26:26 +0100

    bash script to select lang

--- output.sh
+++ output.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
-cp main.pdf CV-IT-ES.pdf
+
+echo "Selecciona el idioma:"
+options=("Castellano" "Inglés" "Alemán" "Catalán")
+select lang in "${options[@]}"
+do
+    case $lang in
+        "Castellano")
+            lang=0
+            break
+            ;;
+        "Inglés")
+            lang=1
+            break
+            ;;
+        "Alemán")
+            lang=2
+            break
+            ;;
+        "Catalán")
+            lang=3
+            break
+            ;;
+        *) echo "Opción inválida";;
+    esac
+done
+
+pdflatex -jobname=main "\def\lang{$lang} \input{main.tex}"
+
+#cp main.pdf CV-IT-ES.pdf
 #echo "Hola Mundo"
